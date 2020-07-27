17913afc2447e3a141391520a8a77ed68e4ea5ec
choiminjeong <61730105+choiminjeong@users.noreply.github.com>
2020-07-27 20:06:23 +0900

    Update views.py
    index부분 지우기

--- djangostagram/dsuser/views.py
+++ djangostagram/dsuser/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import Http404
 from django.views.generic.edit import FormView
 from django.views.generic import ListView
 from .forms import RegisterForm, LoginForm
 from django.core.paginator import Paginator
-from django.utils.decorators import method_decorator
-from dsuser.decorators import login_required
 from post.models import Post
 
-# Create your views here.
 
-# def index(request):
-#     return render(request, 'base.html', 
-#     {'userid' : request.session.get('user')})
-
-
-# class TimelineView(FormView):
-#     template_name = 'base.html'
-#     form_class = TimelineForm
-#     success_url ='/'
 
 class RegisterView(FormView):
     template_name = 'register.html'
@@ -41,13 +28,6 @@ def logout(request):
     return redirect('/')
 
 
-# class Timeline(ListView):
-#     model = Post
-#     template_name = 'index.html'
-#     context_object_name = 'post_list'
-    # paginate_by = 4
-    # paginate_orphans = 4
-    # ordering = 'created'
 
 
 def post_list(request):
@@ -57,5 +37,5 @@ def post_list(request):
 
     posts = paginator.get_page(page)
     userid = request.session.get('user')
-    return render(request, 'index.html', {'posts': posts ,'userid': userid})
+    return render(request, 'timeline.html', {'posts': posts ,'userid': userid})
 
